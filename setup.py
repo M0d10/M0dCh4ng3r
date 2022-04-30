350a8e4695e4d6e99d64d71df6cf83b446d516de
M0d10 <91567560+M0d10@users.noreply.github.com>
2022-04-30 18:23:13 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+import os
+import time
+
+if not 'SUDO_UID':
+  exit("RUN THE SCRIPT AS ROOT")
+
+print("Setting Up.......")
+os.system("sudo chown root:root macchanger.py")
+os.system("pip3 install getmac")
+time.sleep(2)
+print("Finishing Up.......")
+os.system("sudo chmod 700 macchanger.py")
+time.sleep(2)
+print("Finished Setting Up!")
